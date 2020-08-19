402155b2bb4a1d40ef1d4e0e671c6214d32c2f90
Henrique Black <67487181+hnrqblck@users.noreply.github.com>
2020-08-19 15:38:30 -0300

    INSERT//REGISTER
    Usuario Controller created
    INSERT option to database
    REGISTER view page

---
+++ Controllers/UsuarioController.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Cadastros.Models;
+using Microsoft.AspNetCore.Http;
+
+namespace Cadastros.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        public IActionResult Cadastro()
+        {
+            return View();
+        }
+        [HttpPost] //serve para diferenciá-la da outra ação Cadastro() e significa que ela só será chamada por meio de uma requisição do tipo Post
+        public IActionResult Cadastro(Usuario u)
+        {
+            UsuarioRepository ur = new UsuarioRepository();
+            ur.Insert(u);
+            ViewBag.Mensagem = "Usuario Cadastrado com sucesso!";
+            return View();
+        }
+    }
+}
